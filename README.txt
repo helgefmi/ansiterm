de247a47907d0c5b08603505ca993fbec5e14e62
Helge Milde <helge@monsternett.no>
2010-11-12 20:26:44 +0100

    Completely redesigned how escape sequences was parsed

--- README.txt
+++ README.txt
@@ -2,19 +2,16 @@ A virtual terminal for parsing ANSI escape codes in python..
 Call feed() with input and get back output with get_string() or get_tiles(). Supports colors, bold and reverse, and most of the escape codes.
 Works with the game nethack, but i haven't tested it with anything else yet.
 
-helge@helge:~/annet/ansiterm$ telnet nethack.alt.org | tail -n1 > input.txt
+$ telnet nethack.alt.org | tail -n1 > input.txt
 Connection closed by foreign host.
-helge@helge:~/annet/ansiterm$ python
-Python 2.6.6 (r266:84292, Sep 15 2010, 15:52:39)
-[GCC 4.4.5] on linux2
-Type "help", "copyright", "credits" or "license" for more information.
+$ python
 >>> input = open('input.txt', 'r').read()
 >>> repr(input)
 "'\\x1b[2J\\x1b(B\\x1b)0\\x1b[?1049h\\x1b[1;42r\\x1b[m\\x1b[4l\\x1b[?1h\\x1b=\\x1b[39;49m\\x1b[39;49m\\x1b[m\\x1b[H\\x1b[J\\x1b[42;1H\\x1b[?1049l\\r\\x1b[?1l\\x1b>\\x1b[2J\\x1b[?1h\\x1b=\\x1b[?1h\\x1b=\\x1b[?1049h\\x1b[1;42r\\x1b[39;49m\\x1b[m\\x1b[4l\\x1b[H\\x1b[J\\x1b[H\\x1b[J\\x1b[1B ## nethack.alt.org - http://nethack.alt.org/\\r\\x1b[1B ##\\x1b[1B\\x08\\x08## Games on this server are recorded for in-progress viewing and playback!\\x1b[6;3HNot logged in.\\x1b[8;3Hl) Login\\x1b[9;3Hr) Register new user\\x1b[10;3Hw) Watch games in progress\\x1b[12;3Hs) server info\\x1b[13;3Hm) MOTD/news (updated: 2010.09.22)\\x1b[15;3Hq) Quit\\x1b[19;3H=> '"
 >>> import ansiterm
 >>> term = ansiterm.Ansiterm(25, 80)
 >>> term.feed(input)
->>> output = '\n'.join(term.get_string(y * 80, y * 80 + 79) for y in xrange(25))
+>>> output = '\n'.join(term.get_string(y * 80, y * 80 + 80) for y in xrange(25))
 >>> print output
 
  ## nethack.alt.org - http://nethack.alt.org/
--- ansiterm.py
+++ ansiterm.py
@@ -1,7 +1,7 @@
 import re
 
 class Tile:
-    """Represent a single tile in the terminal"""
+    """Represents a single tile in the terminal"""
     def __init__(self):
         self.reset()
     
@@ -50,6 +50,7 @@ class Ansiterm:
         return self.cursor.copy()
 
     def _parse_sgr(self, param):
+        """Handles <escape code>n[;k]m, which changes the graphic rendition"""
         if param >= 30 and param <= 37:
             self.color['fg'] = param
         elif param >= 40 and param <= 47:
@@ -58,118 +59,114 @@ class Ansiterm:
             self.color['bold'] = True
         elif param == 7:
             self.color['reverse'] = True
+        elif param == 0:
+            self.color['fg'] = 37
+            self.color['bg'] = 40
+            self.color['bold'] = False
+            self.color['reverse'] = False
         else:
             return False
         return True
 
+    def _fix_cursor(self):
+        """Makes sure the cursor are within the boundaries of the current terminal size"""
+        while self.cursor['x'] >= self.cols:
+            self.cursor['y'] += 1
+            self.cursor['x'] = self.cursor['x'] - self.cols
+
+        if self.cursor['y'] >= self.rows:
+            self.cursor['y'] = self.rows - 1
+
     def feed(self, input):
-        """
-            Feed the terminal with input.
-            This is where ANSI escape codes are parsed.
-        """
+        """Feeds the terminal with input."""
+        re_getnumber = re.compile(r'^(\d+)')
         while len(input) > 0:
-            # TODO: We must put this somewhere else, because this will not reset the
-            #       cursor if the last input element will leave the cursor in an
-            #       invalid position.
-            while self.cursor['x'] >= self.cols:
-                self.cursor['y'] += 1
-                self.cursor['x'] = self.cursor['x'] - self.cols
-
-            if self.cursor['y'] >= self.rows:
-                self.cursor['y'] = self.rows - 1
-
             # Translate the cursor into an index into our 1-dimensional tileset.
             curidx = self.cursor['y'] * self.cols + self.cursor['x']
-            if input.startswith('\x1b['): # Signature of an ANSI escape code
-                input = input[2:]
-
-                # Check against serveral ANSI escape codes, and make sure
-                # that any successfully parsed input will be followed by a
-                # `continue`.
-                match = re.compile('^(\d+);(\d+)(\w)').match(input)
-                if match:
-                    a, b, c = match.groups()
-                    c = c.lower()
-                    if c == 'h':
-                        self.cursor['y'] = int(a) - 1
-                        self.cursor['x'] = int(b) - 1
-                    elif c == 'r':
-                        pass
-                    elif c == 'm':
-                        self._parse_sgr(int(a))
-                        self._parse_sgr(int(b))
-                    else:
-                        raise Exception('Unknown escape code: \\x1b[' + a + ';' + b + c)
-
-                    input = input[len(a + b + c) + 1:]
-                    continue
-
-                match = re.compile('^(\d+)(\w)').match(input)
-                if match:
-                    a, b = match.groups()
-                    a = int(a)
-                    b = b.lower()
-                    if b == 'j':
-                        if a == 0:
-                            for i in xrange(curidx, self.cols - self.cursor['x'] - 1):
-                                self.tiles[i].reset()
-                        elif a == 1:
-                            for i in xrange(curidx):
-                                self.tiles[i].reset()
-                        elif a == 2:
-                            for i in xrange(self.cols * self.rows - 1):
-                                self.tiles[i].reset()
-                        else:
-                            raise Exception('Unknown escape code: \\x1b[' + str(a) +  b)
-                    elif b == 'b':
-                        self.cursor['y'] += a
-                    elif b == 'm':
-                        if not self._parse_sgr(a):
-                            raise Exception('Unknown color')
-                    elif b == 'l':
-                        pass
-                    else:
-                        raise Exception('Unknown escape code: \\x1b[' + str(a) + b)
-
-                    input = input[len(str(a) + b):]
-                    continue
-
-                match = re.compile('^(\w)').match(input)
-                if match:
-                    a = match.groups()[0].lower()
-                    if a == 'h':
-                        self.cursor['y'] = 0
-                        self.cursor['x'] = 0
-                    elif a == 'k':
-                        for i in xrange(curidx, curidx + self.cols - self.cursor['x'] - 1):
-                            self.tiles[i].reset()
-                    elif a == 'j':
-                        for i in xrange(curidx, self.cols * self.rows - 1):
-                            self.tiles[i].reset()
-                    elif a == 'm':
-                        self.color['fg'] = 37
-                        self.color['bg'] = 40
-                        self.color['bold'] = False
-                        self.color['reverse'] = False
-                    elif a == 'a':
-                        self.cursor['y'] = self.cursor['y'] - 1
-                    elif a == 'b':
-                        self.cursor['y'] = self.cursor['y'] + 1
-                    elif a == 'c':
-                        self.cursor['x'] = self.cursor['x'] + 1
-                    elif a == 'd':
-                        self.cursor['x'] = self.cursor['x'] - 1
-                    else:
-                        raise Exception('Unknown escape code: \\x1b[' + a)
-
+            if input[0] == '\x1b': # Signature of an ANSI escape code
+                input = input[1:]
+                # Sometimes \x1b isn't followed by [, so we check to make sure
+                if input and input[0] == '[':
                     input = input[1:]
-                    continue
+                
+                # This section parses the input into the numeric arguments and
+                # the type of sequence. If not numeric arguments are supplied,
+                # we manually insert a 0.
+                #
+                # Example 1: \x1b[1;37;40m -> numbers=[1, 37, 40] char=m
+                # Example 2: \x1b[m = numbers=[0] char=m
+
+                numbers = []
+                while True: # untill there's no numbers left ..
+                    match = re_getnumber.match(input)
+                    if match:
+                        number = match.groups()[0]
+                        input = input[len(number):]
+                        numbers.append(int(number))
+
+                        if input[0] == ';':
+                            input = input[1:]
+                    else:
+                        break
+                char, input = input[0], input[1:]
+
+                if len(numbers) == 0:
+                    numbers.append(0)
+
+                # Check for known escape sequences and execute them.
+                if char == 'H': # Sets cursor
+                    if len(numbers) == 1: # (\x1b[H -> \x1b[1;1H)
+                        assert numbers[0] == 0
+                        numbers = [1, 1]
+                    
+                    self.cursor['y'] = numbers[0] - 1 # 1-based indexes
+                    self.cursor['x'] = numbers[1] - 1 #
+                elif char == 'm' or char == 'M': # Sets SGR parameters
+                    for num in numbers:
+                        self._parse_sgr(num)
+                elif char == 'J': # Clears (parts of) the screen.
+                    # From cursor to end of screen
+                    if numbers[0] == 0:
+                        range_ = (curidx, self.cols - self.cursor['x'] - 1)
+                    # From beginning to cursor
+                    elif numbers[0] == 1:
+                        range_ = (0, curidx)
+                    # Clears the whole screen
+                    elif numbers[0] == 2:
+                        range_ = (0, self.cols * self.rows - 1)
+                    else:
+                        raise Exception('Unknown argument for J parameter: %s (input=%r)' % (numbers, input[:20]))
+                    for i in xrange(*range_):
+                        self.tiles[i].reset()
+                elif char == 'K': # Clears (parts of) the line
+                    if numbers[0] == 0:
+                        range_ = (curidx, curidx + self.cols - self.cursor['x'] - 1)
+                    elif numbers[0] == 1:
+                        range_ = (curidx % self.cols, curidx)
+                    elif numbers[0] == 2:
+                        range_ = (curidx % self.cols, curidx % self.cols + self.cols)
+                    else:
+                        raise Exception('Unknown argument for K parameter: %s (input=%r)' % (numbers, input[:20]))
+                    for i in xrange(*range_):
+                        self.tiles[i].reset()
+                elif char == 'A': # Move cursor up
+                    self.cursor['y'] -= 1 if not numbers else (numbers[0] + 1)
+                elif char == 'B': # Move cursor down
+                    self.cursor['y'] += 1 if not numbers else (numbers[0] + 1)
+                elif char == 'C': # Move cursor right
+                    self.cursor['x'] += 1 if not numbers else (numbers[0] + 1)
+                elif char == 'D': # Move cursor left
+                    self.cursor['x'] -= 1 if not numbers else (numbers[0] + 1)
+                elif char == 'r' or char == 'l': # TODO
+                    pass
+                else:
+                    raise Exception('Unknown escape code: char=%r numbers=%r input=%r' % (char, numbers, input[:20]))
             else:
                 # If we end up here, the character should should just be
                 # added to the current tile and the cursor should be updated.
                 # Some characters such as \r, \n will only affect the cursor.
                 # TODO: Find out exactly what should be accepted here
-                #       i.e. \xff should probably be ignored.
+                #       i.e. only ASCII-7?
                 a = input[0]
                 if a == '\r':
                     self.cursor['x'] = 0
@@ -184,3 +181,4 @@ class Ansiterm:
                     self.cursor['x'] += 1
 
                 input = input[1:]
+        self._fix_cursor()
