b0498fdd4970e86431b495c5a554ddd751edf931
Helge Milde <helge@monsternett.no>
2010-11-13 14:22:23 +0100

    Rewrote the sequence-part of feed() into _parse_sequence and _evaluate_sequence

--- ansiterm.py
+++ ansiterm.py
@@ -22,7 +22,7 @@ class Tile:
         self.color['bold'] = color['bold']
 
 class Ansiterm:
-    escape_parser = re.compile(r'^\x1b\[?([\d;]*)(\w)')
+    _escape_parser = re.compile(r'^\x1b\[?([\d;]*)(\w)')
     def __init__(self, rows, cols):
         """Initializes the ansiterm with rows*cols white-on-black spaces"""
         self.rows = rows
@@ -78,82 +78,107 @@ class Ansiterm:
         if self.cursor['y'] >= self.rows:
             self.cursor['y'] = self.rows - 1
 
+    def _parse_sequence(self, input):
+        """
+        This section parses the input into the numeric arguments and
+        the type of sequence. If no numeric arguments are supplied,
+        we manually insert a 0 or a 1 depending on the sequence type,
+        because different types have different default values.
+        
+        Example 1: \x1b[1;37;40m -> numbers=[1, 37, 40] char=m
+        Example 2: \x1b[m = numbers=[0] char=m
+        """
+        if input[0] != '\x1b':
+            return None, input
+
+        match = Ansiterm._escape_parser.match(input)
+        if not match:
+            raise Exception('Invalid escape sequence, input[:20]=%r' % input[:20])
+
+        args, char = match.groups()
+        # If arguments are omitted, add the default argument for this sequence.
+        if len(args) == 0:
+            if char in 'ABCDEFSTf':
+                numbers = [1]
+            elif char == 'H':
+                numbers = [1, 1]
+            else:
+                numbers = [0]
+        else:
+            numbers = map(int, args.split(';'))
+
+        return (char, numbers), input[match.end():]
+
+    def _evaluate_sequence(self, char, numbers):
+        """
+        Evaluates a sequence (i.e., this changes the state of the terminal).
+        Is meant to be called with the return values from _parse_sequence as arguments.
+        """
+        # Translate the cursor into an index into our 1-dimensional tileset.
+        curidx = self.cursor['y'] * self.cols + self.cursor['x']
+
+        # Sets cursor position
+        if char == 'H':
+            self.cursor['y'] = numbers[0] - 1 # 1-based indexes
+            self.cursor['x'] = numbers[1] - 1 #
+        # Sets color/boldness
+        elif char == 'm' or char == 'M':
+            for num in numbers:
+                self._parse_sgr(num)
+        # Clears (parts of) the screen.
+        elif char == 'J':
+            # From cursor to end of screen
+            if numbers[0] == 0:
+                range_ = (curidx, self.cols - self.cursor['x'] - 1)
+            # From beginning to cursor
+            elif numbers[0] == 1:
+                range_ = (0, curidx)
+            # The whole screen
+            elif numbers[0] == 2:
+                range_ = (0, self.cols * self.rows - 1)
+            else:
+                raise Exception('Unknown argument for J parameter: %s (input=%r)' % (numbers, input[:20]))
+            for i in xrange(*range_):
+                self.tiles[i].reset()
+        # Clears (parts of) the line
+        elif char == 'K':
+            # From cursor to end of line
+            if numbers[0] == 0:
+                range_ = (curidx, curidx + self.cols - self.cursor['x'] - 1)
+            # From beginning of line to cursor
+            elif numbers[0] == 1:
+                range_ = (curidx % self.cols, curidx)
+            # The whole line
+            elif numbers[0] == 2:
+                range_ = (curidx % self.cols, curidx % self.cols + self.cols)
+            else:
+                raise Exception('Unknown argument for K parameter: %s (input=%r)' % (numbers, input[:20]))
+            for i in xrange(*range_):
+                self.tiles[i].reset()
+        # Move cursor up
+        elif char == 'A':
+            self.cursor['y'] -= 1 if not numbers else (numbers[0])
+        # Move cursor down
+        elif char == 'B':
+            self.cursor['y'] += 1 if not numbers else (numbers[0])
+        # Move cursor right
+        elif char == 'C':
+            self.cursor['x'] += 1 if not numbers else (numbers[0])
+        # Move cursor left
+        elif char == 'D':
+            self.cursor['x'] -= 1 if not numbers else (numbers[0])
+        elif char == 'r' or char == 'l': # TODO
+            pass
+        else:
+            raise Exception('Unknown escape code: char=%r numbers=%r input=%r' % (char, numbers, input[:20]))
+
     def feed(self, input):
         """Feeds the terminal with input."""
         while len(input) > 0:
-            # Translate the cursor into an index into our 1-dimensional tileset.
-            curidx = self.cursor['y'] * self.cols + self.cursor['x']
-            if input[0] == '\x1b': # Signature of an ANSI escape code
-                # This section parses the input into the numeric arguments and
-                # the type of sequence. If no numeric arguments are supplied,
-                # we manually insert a 0 or a 1 depending on the sequence type,
-                # because different types have different default values.
-                #
-                # Example 1: \x1b[1;37;40m -> numbers=[1, 37, 40] char=m
-                # Example 2: \x1b[m = numbers=[0] char=m
-                match = Ansiterm.escape_parser.match(input)
-                if not match:
-                    raise Exception('Invalid escape sequence, input[:20]=%r' % input[:20])
-                input = input[match.end():]
-                args, char = match.groups()
-                # If arguments are omitted, add the default argument for this sequence.
-                if len(args) == 0:
-                    if char in 'ABCDEFSTf':
-                        numbers = [1]
-                    elif char == 'H':
-                        numbers = [1, 1]
-                    else:
-                        numbers = [0]
-                else:
-                    numbers = map(int, args.split(';'))
-
-                # Check for known escape sequences and execute them.
-                if char == 'H': # Sets cursor
-                    self.cursor['y'] = numbers[0] - 1 # 1-based indexes
-                    self.cursor['x'] = numbers[1] - 1 #
-                elif char == 'm' or char == 'M': # Sets color/boldness
-                    for num in numbers:
-                        self._parse_sgr(num)
-                elif char == 'J': # Clears (parts of) the screen.
-                    # From cursor to end of screen
-                    if numbers[0] == 0:
-                        range_ = (curidx, self.cols - self.cursor['x'] - 1)
-                    # From beginning to cursor
-                    elif numbers[0] == 1:
-                        range_ = (0, curidx)
-                    # The whole screen
-                    elif numbers[0] == 2:
-                        range_ = (0, self.cols * self.rows - 1)
-                    else:
-                        raise Exception('Unknown argument for J parameter: %s (input=%r)' % (numbers, input[:20]))
-                    for i in xrange(*range_):
-                        self.tiles[i].reset()
-                elif char == 'K': # Clears (parts of) the line
-                    # From cursor to end of line
-                    if numbers[0] == 0:
-                        range_ = (curidx, curidx + self.cols - self.cursor['x'] - 1)
-                    # From beginning of line to cursor
-                    elif numbers[0] == 1:
-                        range_ = (curidx % self.cols, curidx)
-                    # The whole line
-                    elif numbers[0] == 2:
-                        range_ = (curidx % self.cols, curidx % self.cols + self.cols)
-                    else:
-                        raise Exception('Unknown argument for K parameter: %s (input=%r)' % (numbers, input[:20]))
-                    for i in xrange(*range_):
-                        self.tiles[i].reset()
-                elif char == 'A': # Move cursor up
-                    self.cursor['y'] -= 1 if not numbers else (numbers[0])
-                elif char == 'B': # Move cursor down
-                    self.cursor['y'] += 1 if not numbers else (numbers[0])
-                elif char == 'C': # Move cursor right
-                    self.cursor['x'] += 1 if not numbers else (numbers[0])
-                elif char == 'D': # Move cursor left
-                    self.cursor['x'] -= 1 if not numbers else (numbers[0])
-                elif char == 'r' or char == 'l': # TODO
-                    pass
-                else:
-                    raise Exception('Unknown escape code: char=%r numbers=%r input=%r' % (char, numbers, input[:20]))
+            # If the input starts with \x1b, try to parse end evaluate a sequence.
+            parsed, input = self._parse_sequence(input)
+            if parsed:
+                self._evaluate_sequence(*parsed)
             else:
                 # If we end up here, the character should should just be
                 # added to the current tile and the cursor should be updated.
